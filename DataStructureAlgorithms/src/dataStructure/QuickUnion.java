dc1dfb0521b3276d26721e7853a62bfb699a8e85
ram <ram@ram-HP-Pavilion-TS-Sleekbook-14>
2018-08-08 11:02:19 +0530

    commit 8/8/18 -2

--- DataStructureAlgorithms/src/dataStructure/QuickUnion.java
+++ DataStructureAlgorithms/src/dataStructure/QuickUnion.java
@@ -1,7 +1,51 @@
 package dataStructure;
 
 public class QuickUnion {
-	
-	
 
+	private int [] arr;
+
+	public QuickUnion(int n) {
+		arr = new int [n];
+		for(int i =0; i<n ;i++)
+			arr[i] = i;
+	}
+
+	int findRoot(int element) {
+		int root = arr[element];
+		while(arr[root] != root) {
+			root = arr[root];
+		}
+		return root;
+	}
+
+	boolean connected(int x, int y) {
+		int root_x = findRoot(x);
+		int root_y = findRoot(y);
+		return(root_x == root_y);
+	}
+
+	public void union(int x, int y) {
+		int root_x = findRoot(x);
+		int root_y = findRoot(y);
+		arr[root_x] = root_y;
+	}
+
+	public void display() {
+		for(int i=0; i<arr.length; i++)
+			System.out.println("Index : "+i +" Value :"+arr[i]);
+	}
+
+	public static void main(String[] args) {
+
+		QuickUnion a = new QuickUnion(10);
+		a.display();
+		a.union(3, 8);
+		System.out.println("*******");
+		a.display();
+		a.union(3, 9);
+		System.out.println("*******");
+		a.display();
+		System.out.println(a.connected(8,7));
+		
+	}
 }
